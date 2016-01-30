4a5a5adbfbfcd52626a636912ff748280dce947d
joemanley201 <joemanley201@gmail.com>
2016-01-30 13:57:39 -0800

    Delegate constants

---
+++ blockly_constants.py
@@ -0,0 +1,8 @@
+#File Names
+blocklyOutputFileName = "js/blocks/output.js"
+logFileName = "blocklyGenerator.log"
+
+#Block Types
+PROOF_STATEMENT = "proofStatement"
+PROOF_END = "proofEnd"
+PROOF_STEP = "proofStep"
