65b33b06a12dff505b26d04bd281b0bdd74d484b
joemanley201 <joemanley201@gmail.com>
2016-01-29 18:28:49 -0800

    Generate PNG from HTML_CSS MathJax

--- markdownGenerator/js/core.js
+++ markdownGenerator/js/core.js
@@ -2,47 +2,21 @@ var rowCounter = 1;
 var blocks;
 
 
-
-function svg_to_png_data(target) {
-  var ctx, mycanvas, svg_data, img, child;
-
-  // Flatten CSS styles into the SVG
-  for (i = 0; i < target.childNodes.length; i++) {
-    child = target.childNodes[i];
-    var cssStyle = window.getComputedStyle(child);
-    if(cssStyle){
-       child.style.cssText = cssStyle.cssText;
-    }
-  }
-
-  // Construct an SVG image
-  svg_data = '<svg xmlns="http://www.w3.org/2000/svg" width="' + target.offsetWidth +
-             '" height="' + target.offsetHeight + '">' + target.innerHTML + '</svg>';
-  img = new Image();
-  img.src = "data:image/svg+xml," + encodeURIComponent(svg_data);
-
-  // Draw the SVG image to a canvas
-  mycanvas = document.createElement('canvas');
-  mycanvas.width = target.offsetWidth;
-  mycanvas.height = target.offsetHeight;
-  ctx = mycanvas.getContext("2d");
-  ctx.drawImage(img, 0, 0);
-
-  // Return the canvas's data
-  return mycanvas.toDataURL("image/png");
-}
-
-// Takes an SVG element as target
-function svg_to_png_replace(target) {
-  var data, img;
-  data = svg_to_png_data(target);
-  img = new Image();
-  img.src = data;
-  target.parentNode.replaceChild(img, target);
-}
-
 function generateImage() {
-	svg_to_png_replace(document.querySelector('svg'));
+	//var input = document.getElementsByTagName("svg")[1];
+	var input = document.getElementById("mathJaxExpression");
+    var canvas = document.getElementById("canvas");
+	var draw = function () {
+        rasterizeHTML.drawHTML(input.innerHTML, canvas).then(function (result) {
+            console.log(result);
+            console.log(result.svg);
+            $("#download")[0].href = document.getElementById("canvas").toDataURL();
+            $("#download")[0].download = "hee.png"
+        }, function (e) {
+            console.log('An error occured:', e);
+        });
+    };
+    draw();
 }
 
 function writeToTextFile(content){
